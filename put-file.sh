514fac761923b8a33f887e4d0f7aef1499275c73
Francisco Cardoso <francardoso@outlook.com>
2021-11-17 16:00:15 -0300

    Explaining put-file.sh

--- put-file.sh
+++ put-file.sh
@@ -1,2 +1,3 @@
+# This is a testing script for uploading a car file to API Gateway through CURL
 # Replace host (okhpnoo7vb.execute-api.us-east-2.amazonaws.com/v1/) with AWS API Gateway provided URL
 curl -v -X PUT https://okhpnoo7vb.execute-api.us-east-2.amazonaws.com/v1/carfile.car --upload-file carfile.car
