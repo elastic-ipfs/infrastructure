e26876a4835e1625ea6e8898b7aa1cef1c77aa35
Francisco Cardoso <francardoso@outlook.com>
2021-11-19 16:53:25 -0300

    Policy for DynamoDB cars table

--- terraform/modules/dynamodb/auth.tf
+++ terraform/modules/dynamodb/auth.tf
@@ -22,7 +22,37 @@ resource "aws_iam_policy" "dynamodb_cid_policy" {
 EOF
 }
 
+resource "aws_iam_policy" "dynamodb_car_policy" {
+  name        = "dynamodb-car-policy"
+  description = "Policy for allowing all Dynamodb Actions for CAR table"
+  policy      = <<EOF
+{  
+  "Version": "2012-10-17",
+  "Statement":[{
+    "Effect": "Allow",
+    "Action": [
+     "dynamodb:BatchGetItem",
+     "dynamodb:GetItem",
+     "dynamodb:Query",
+     "dynamodb:Scan",
+     "dynamodb:BatchWriteItem",
+     "dynamodb:PutItem",
+     "dynamodb:UpdateItem"
+    ],
+    "Resource": "${aws_dynamodb_table.cars_table.arn}"
+   }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role_policy_attachment" "dynamodb_cid_policy_attach" {
   role       = var.lambdaRoleName
   policy_arn = aws_iam_policy.dynamodb_cid_policy.arn
 }
+
+resource "aws_iam_role_policy_attachment" "dynamodb_car_policy_attach" {
+  role       = var.lambdaRoleName
+  policy_arn = aws_iam_policy.dynamodb_car_policy.arn
+}
+
