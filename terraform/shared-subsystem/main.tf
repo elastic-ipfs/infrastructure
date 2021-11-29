3351ecd7dbcd928f54ba5c52a49aeb8a79ac614d
Francisco Cardoso <francardoso@outlook.com>
2021-11-29 16:51:56 -0300

    shared subsystem main

---
+++ terraform/shared-subsystem/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "s3" {
+    profile = "ipfs"
+    bucket         = "ipfs-aws-terraform-state"
+    dynamodb_table = "ipfs-aws-terraform-state-lock"
+    region         = "us-west-2"
+    key            = "terraform.shared.tfstate"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.38"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  profile = "ipfs"
+  region  = "us-west-2"
+  default_tags {
+    tags = {
+      Team        = "NearForm"
+      Project     = "AWS-IPFS"
+      Environment = "POC"
+      Subsystem   = "Shared"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "cars" {
+  bucket = var.carsBucketName
+  acl    = "private"
+  object_lock_configuration {
+    object_lock_enabled = "Enabled"
+  }  
+}
+
+
+# DynamoDB
+module "dynamodb" {
+  source = "../modules/dynamodb"
+  roleName=module.lambda-from-s3.lambdaRoleName
+#   enableCidTable=true
+#   enableCarTable=true
+}
