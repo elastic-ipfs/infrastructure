736c2424223de43d9eeb34cdf08fc1828d746bab
Francisco Cardoso <francardoso@outlook.com>
2021-11-30 10:05:06 -0300

    dynamodb policy outputs

--- terraform/modules/dynamodb/outputs.tf
+++ terraform/modules/dynamodb/outputs.tf
@@ -1,8 +1,7 @@
-## TODO: Não acho que vai precisar, eu vou buscar direto do remote state
-# output "dynamodb_cid_policy" {
-#   value = aws_iam_policy.dynamodb_cid_policy
-# }
+output "dynamodb_cid_policy" {
+  value = aws_iam_policy.dynamodb_cid_policy
+}
 
-# output "dynamodb_car_policy" {
-#   value = aws_iam_policy.dynamodb_car_policy
-# }
+output "dynamodb_car_policy" {
+  value = aws_iam_policy.dynamodb_car_policy
+}
