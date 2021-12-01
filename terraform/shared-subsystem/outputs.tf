b3e0c37b494c23b9267f773bd8cebf1e9badde15
Francisco Cardoso <francardoso@outlook.com>
2021-12-01 10:13:53 -0300

    Cleanning shared module outputs

--- terraform/shared-subsystem/outputs.tf
+++ terraform/shared-subsystem/outputs.tf
@@ -1,11 +1,22 @@
 output "cars_bucket" {
-  value = aws_s3_bucket.cars
+  value = {
+    bucket = aws_s3_bucket.cars.bucket
+    id = aws_s3_bucket.cars.id
+    arn = aws_s3_bucket.cars.arn
+    region = aws_s3_bucket.cars.region
+  }
 }
 
 output "dynamodb_cid_policy" {
-  value = module.dynamodb.dynamodb_cid_policy
+  value = {
+    name = module.dynamodb.dynamodb_cid_policy.name,
+    arn  = module.dynamodb.dynamodb_cid_policy.arn,
+  }
 }
 
 output "dynamodb_car_policy" {
-  value = module.dynamodb.dynamodb_car_policy
+  value = {
+    name = module.dynamodb.dynamodb_car_policy.name,
+    arn  = module.dynamodb.dynamodb_car_policy.arn,
+  }
 }
