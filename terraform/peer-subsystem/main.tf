473ecc5def20f81ab9834d085b96153635354c11
Francisco Cardoso <francardoso@outlook.com>
2021-12-07 17:15:13 -0300

    Test this commit should not trigger auto action

--- terraform/peer-subsystem/main.tf
+++ terraform/peer-subsystem/main.tf
@@ -161,3 +161,4 @@ module "kube-specs" {
   token                   = data.aws_eks_cluster_auth.eks.token
   cluster_ca_certificate  = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
 }
+
