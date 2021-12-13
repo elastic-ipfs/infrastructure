c4d2fbe0fd53aa6a4f3de11dab8c28eca783cf60
Francisco Cardoso <francardoso@outlook.com>
2021-12-13 09:08:23 -0300

    verify false

--- terraform/modules/kube-specs/helm.tf
+++ terraform/modules/kube-specs/helm.tf
@@ -8,7 +8,7 @@ provider "helm" {
 
 resource "helm_release" "metric-server" {
   name       = "metric-server-release"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://charts.bitnami.com/bitnami" # TODO: Bitnami repo var
   chart      = "metrics-server"
   namespace  = "kube-system"
 
@@ -22,10 +22,11 @@ resource "helm_release" "nginix_ingress" {
     name      = "nginix_ingress"
     # repository = "https://kubernetes-charts.storage.googleapis.com"
     # repository = "https://helm.nginx.com/stable"
-    repository = " https://charts.bitnami.com/bitnami"
+    repository = " https://charts.bitnami.com/bitnami" # TODO: Bitnami repo var
     # chart     = "nginx-stable/nginx-ingress"
     # chart     = "stable/nginx-ingress"
     # chart     = "bitnami/nginx"
     chart     = "nginx"
     namespace = "kube-system"
+    verify = false
 }
