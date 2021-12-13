60f3b57113aa0c92d58cfbba95755cbedc70a095
Francisco Cardoso <francardoso@outlook.com>
2021-12-13 18:36:17 -0300

    Default backend

--- terraform/modules/kube-specs/ing.tf
+++ terraform/modules/kube-specs/ing.tf
@@ -1,30 +1,23 @@
-# resource "kubernetes_ingress_class_v1" "aws_ipfs_ingress_class" {
-#   metadata {
-#     name = "aws-ipfs-ingress-class"
-#     annotations = { # https://github.com/kubernetes/ingress-nginx/issues/7600
-#       "ingressclass.kubernetes.io/is-default-class" : "true"
-#     }
-#   }
-
-#   spec {
-#     # controller = "k8s.io/ingress-nginx"
-#     controller = "nginx.org/ingress-controller"
-#   }
-# }
-
-
 resource "kubernetes_ingress_v1" "aws_ipfs_ingress" {
   # wait_for_load_balancer = true
   metadata {
     name = "aws-ipfs-ingress"
     annotations = {
-      "ingress.kubernetes.io/rewrite-target": "/"
+      "ingress.kubernetes.io/rewrite-target" : "/"
     }
   }
 
   spec {
     # ingress_class_name = kubernetes_ingress_class_v1.aws_ipfs_ingress_class.metadata[0].name
     ingress_class_name = "nginx"
+    default_backend {
+      service {
+        name = local.service_name
+        port {
+          number = local.service_port
+        }
+      }
+    }
     rule {
       http {
         path {
@@ -36,7 +29,7 @@ resource "kubernetes_ingress_v1" "aws_ipfs_ingress" {
               }
             }
           }
-          path = "/peer"
+          path      = "/peer"
           path_type = "Prefix"
         }
       }
