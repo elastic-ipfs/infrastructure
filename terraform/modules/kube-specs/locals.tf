633d454bc0ab79148f6ab8f32023dd66a9a19b40
Francisco Cardoso <francardoso@outlook.com>
2021-12-15 15:16:36 -0300

    port 3000

--- terraform/modules/kube-specs/locals.tf
+++ terraform/modules/kube-specs/locals.tf
@@ -3,6 +3,6 @@ locals {
   service_account_name      = "irsa"
   bitnami_repo              = "https://charts.bitnami.com/bitnami"
   peer_service_name = "${kubernetes_deployment.peer_deploy.metadata[0].name}"
-  peer_service_port = 80
-  peer_service_target_port = 80
+  peer_service_port = 3000
+  peer_service_target_port = 3000
 }
