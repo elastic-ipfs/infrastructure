24fe49097fe247a335512535c208df9dfb317dcb
Francisco Cardoso <francardoso@outlook.com>
2022-01-03 15:22:10 -0300

    Removing test tag

--- terraform/peer-subsystem/main.tf
+++ terraform/peer-subsystem/main.tf
@@ -73,7 +73,6 @@ module "vpc" {
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                    = "1"
-    "test"                    = "deleteme"
   }
 
   private_subnet_tags = {
