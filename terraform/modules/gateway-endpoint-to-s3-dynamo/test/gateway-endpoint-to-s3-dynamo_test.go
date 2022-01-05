585577d7bc5f3a16f3aa1e87e33f3ce03b35fc1a
Francisco Cardoso <francardoso@outlook.com>
2022-01-05 17:03:30 -0300

    Route table association validation

--- terraform/modules/gateway-endpoint-to-s3-dynamo/test/gateway-endpoint-to-s3-dynamo_test.go
+++ terraform/modules/gateway-endpoint-to-s3-dynamo/test/gateway-endpoint-to-s3-dynamo_test.go
@@ -36,6 +36,7 @@ func TestTerraformAwsDynamoDBExample(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	vpcId := terraform.Output(t, terraformOptions, "vpc_id")
+	privateRouteTableIds := terraform.OutputList(t, terraformOptions, "private_route_table_ids")
 	vpcEndpointS3 := terraform.OutputMap(t, terraformOptions, "aws_vpc_endpoint_s3")
 	vpcEndpointDynamodb := terraform.OutputMap(t, terraformOptions, "aws_vpc_endpoint_dynamodb")
 	vpcEndpointRouteAssociationS3 := terraform.OutputMap(t, terraformOptions, "aws_vpc_endpoint_route_table_association_s3")
@@ -53,7 +54,6 @@ func TestTerraformAwsDynamoDBExample(t *testing.T) {
 		panic("DescribeVpcEndpoints error, " + err.Error())
 	}
 
-	// TODO: Check if association with route table
 	assert.Equal(t, fmt.Sprintf("com.amazonaws.%s.s3", awsRegion), vpcEndpointS3["service_name"])
 	assert.Equal(t, fmt.Sprintf("com.amazonaws.%s.dynamodb", awsRegion), vpcEndpointDynamodb["service_name"])
 	assert.Equal(t, "available", vpcEndpointS3["state"])
@@ -62,6 +62,8 @@ func TestTerraformAwsDynamoDBExample(t *testing.T) {
 	assert.Equal(t, vpcId, vpcEndpointDynamodb["vpc_id"])
 	assert.Equal(t, vpcEndpointS3["id"], vpcEndpointRouteAssociationS3["vpc_endpoint_id"])
 	assert.Equal(t, vpcEndpointDynamodb["id"], vpcEndpointRouteAssociationDynamodb["vpc_endpoint_id"])
+	assert.Equal(t, privateRouteTableIds[0], vpcEndpointRouteAssociationS3["route_table_id"])
+	assert.Equal(t, privateRouteTableIds[0], vpcEndpointRouteAssociationDynamodb["route_table_id"])
 	assert.Equal(t, 2, len(vpcEndpointsFromAWSSDK.VpcEndpoints))
 	assert.Equal(t, types.State("available"), vpcEndpointsFromAWSSDK.VpcEndpoints[0].State)
 	assert.Equal(t, types.State("available"), vpcEndpointsFromAWSSDK.VpcEndpoints[1].State)
