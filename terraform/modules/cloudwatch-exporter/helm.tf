9d23825c1881f28ff2cf0ac8a2267b55a5ade4c6
Francisco Cardoso <francardoso@outlook.com>
2022-02-22 10:28:52 -0300

    Fixing cloudwatch annotations and enabling servicemonitor to look at new namespace

--- terraform/modules/cloudwatch-exporter/helm.tf
+++ terraform/modules/cloudwatch-exporter/helm.tf
@@ -22,6 +22,7 @@ resource "helm_release" "cloudwatch_exporter" {
   set {
     name  = "service.labels.metricsMonitor"
     value = "true"
+    type = "string"
   }
 
   set {
@@ -37,12 +38,14 @@ resource "helm_release" "cloudwatch_exporter" {
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = module.cloudwatch_exporter_role.iam_role_arn
+    type = "string"
   }
 
-  # set {
-  #   name  = "eks.amazonaws.com/sts-regional-endpoints"
-  #   value = "true"
-  # }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/sts-regional-endpoints"
+    value = "true"
+    type = "string"
+  }
 
   set {
     name  = "config"
