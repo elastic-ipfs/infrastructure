cf5e4090ab21842c2e00b769cb1f8042bee8f6ba
Francisco Cardoso <francardoso@outlook.com>
2022-02-28 16:42:45 -0300

    Namespace fix

--- terraform/modules/cloudwatch-exporter/helm.tf
+++ terraform/modules/cloudwatch-exporter/helm.tf
@@ -139,42 +139,42 @@ resource "helm_release" "cloudwatch_exporter" {
       - aws_dimensions: 
         - ipfs_provider_component
         aws_metric_name: dynamo-creates-count
-        aws_namespace: indexer-lambda
+        aws_namespace: indexer-lambda-metrics
         aws_statistics:
         - Sum
         - Average 
       - aws_dimensions: 
         - ipfs_provider_component
         aws_metric_name: dynamo-deletes-count
-        aws_namespace: indexer-lambda
+        aws_namespace: indexer-lambda-metrics
         aws_statistics:
         - Sum
         - Average 
       - aws_dimensions: 
         - ipfs_provider_component
         aws_metric_name: dynamo-reads-count
-        aws_namespace: indexer-lambda
+        aws_namespace: indexer-lambda-metrics
         aws_statistics:
         - Sum
         - Average
       - aws_dimensions: 
         - ipfs_provider_component
         aws_metric_name: dynamo-updates-count
-        aws_namespace: indexer-lambda
+        aws_namespace: indexer-lambda-metrics
         aws_statistics:
         - Sum
         - Average
       - aws_dimensions: 
         - ipfs_provider_component
         aws_metric_name: s3-fetchs-count
-        aws_namespace: indexer-lambda
+        aws_namespace: indexer-lambda-metrics
         aws_statistics:
         - Sum
         - Average
       - aws_dimensions: 
         - ipfs_provider_component
         aws_metric_name: sqs-publishes-count
-        aws_namespace: indexer-lambda
+        aws_namespace: indexer-lambda-metrics
         aws_statistics:
         - Sum
         - Average
