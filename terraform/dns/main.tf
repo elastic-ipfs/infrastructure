9670513b31184f7a314e23ffc6b39d818d58f5c4
Francisco Cardoso <francardoso@outlook.com>
2022-03-24 17:12:09 -0300

    Remove uploader cname

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -47,8 +47,4 @@ module "dns_route53" {
   subdomains_bitwsap_loadbalancer = var.subdomains_bitwsap_loadbalancer
   subdomain_apis                  = var.subdomain_apis
   bitswap_load_balancer_hostname  = var.bitswap_load_balancer_hostname
-  api_gateway = {
-    api_id     = data.terraform_remote_state.indexing.outputs.api_id
-    stage_name = data.terraform_remote_state.indexing.outputs.stage_name
-  }
 }
--- terraform/modules/dns-route53/main.tf
+++ terraform/modules/dns-route53/main.tf
@@ -9,12 +9,12 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
-data "aws_route53_zone" "hosted_zone" {
+data "aws_route53_zone" "hosted_zone" { # Existing zone
   count = var.existing_zone ? 1 : 0
   name  = var.domain_name
 }
 
-resource "aws_route53_zone" "hosted_zone" {
+resource "aws_route53_zone" "hosted_zone" { # Non existing zone
   count = var.existing_zone ? 0 : 1
   name  = var.domain_name
 }
@@ -25,33 +25,4 @@ resource "aws_route53_record" "peer_bitswap_load_balancer" {
   type    = "CNAME"
   ttl     = "300"
   records = [var.bitswap_load_balancer_hostname]
-}
-
-### API Gateway
-resource "aws_api_gateway_domain_name" "api" {
-  domain_name              = local.api_domain
-  regional_certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_base_path_mapping" "api" {
-  api_id      = var.api_gateway.api_id
-  stage_name  = var.api_gateway.stage_name
-  base_path   = var.api_gateway.stage_name
-  domain_name = aws_api_gateway_domain_name.api.domain_name
-}
-
-resource "aws_route53_record" "api" {
-  name    = aws_api_gateway_domain_name.api.domain_name
-  type    = "A"
-  zone_id = var.existing_zone ? data.aws_route53_zone.hosted_zone[0].zone_id : aws_route53_zone.hosted_zone[0].zone_id
-
-  alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.api.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.api.regional_zone_id
-  }
 }
