8dfa2ea7762e65c4c56d99e1675c364d04e8ff1f
Francisco Cardoso <francardoso@outlook.com>
2022-03-24 17:30:38 -0300

    [REVIEW] - [NA-000] - Remove API Gateway DNS Entry (#64)
    * Remove uploader cname
    
    * Removing all attachmentes with API Gateway
    
    * Workflow

--- terraform/dns/dns.tfvars
+++ terraform/dns/dns.tfvars
@@ -3,6 +3,5 @@ profile = "ipfs-automation"
 region = "us-west-2"
 existing_aws_zone=true
 aws_domain_name = "ipfs-elastic-provider-aws.com"
-subdomain_apis = "api.uploader"
-subdomains_bitwsap_loadbalancer = "peer"
+subdomains_bitwsap_loadbalancer="peer"
 bitswap_load_balancer_hostname = "ad62259e461cc4df986d3d01bd51140f-2139506693.us-west-2.elb.amazonaws.com"
--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -45,10 +45,6 @@ module "dns_route53" {
   existing_zone                   = var.existing_aws_zone
   domain_name                     = var.aws_domain_name
   subdomains_bitwsap_loadbalancer = var.subdomains_bitwsap_loadbalancer
-  subdomain_apis                  = var.subdomain_apis
   bitswap_load_balancer_hostname  = var.bitswap_load_balancer_hostname
-  api_gateway = {
-    api_id     = data.terraform_remote_state.indexing.outputs.api_id
-    stage_name = data.terraform_remote_state.indexing.outputs.stage_name
-  }
 }
+
