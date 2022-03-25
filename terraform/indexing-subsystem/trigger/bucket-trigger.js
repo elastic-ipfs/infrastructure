b90ca1d3ce087a2985727698e566ff5ea05c1471
Francisco Cardoso <francardoso@outlook.com>
2022-03-25 08:40:18 -0300

    List all objects from S3

---
+++ terraform/indexing-subsystem/trigger/bucket-trigger.js
@@ -0,0 +1,31 @@
+const { S3 } = require('aws-sdk')
+const s3 = new S3()
+
+async function* listAllKeys(opts) {
+  opts = { ...opts }
+  do {
+    const data = await s3.listObjectsV2(opts).promise()
+    opts.ContinuationToken = data.NextContinuationToken
+    yield data
+  } while (opts.ContinuationToken)
+}
+
+const opts = {
+  Bucket: 'francisco-nearform-test' /* required */,
+  // ContinuationToken: 'STRING_VALUE',
+  // Delimiter: 'STRING_VALUE',
+  // EncodingType: url,
+  // FetchOwner: true || false,
+  // MaxKeys: 'NUMBER_VALUE',
+  // Prefix: 'STRING_VALUE',
+  // RequestPayer: requester,
+  // StartAfter: 'STRING_VALUE'
+}
+
+async function main() {
+  // using for of await loop
+  for await (const data of listAllKeys(opts)) {
+    console.log(data.Contents)
+  }
+}
+main()
