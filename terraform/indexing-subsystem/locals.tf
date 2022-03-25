e29eda081b2caa2cfc7f9a5144b7c07fe5a40295
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-03-25 16:04:58 -0300

    creating lambda-from-sqs function

---
+++ terraform/indexing-subsystem/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  indexer_lambda = {
+    name = "indexer"
+  }
+  
+  environment_variables = {
+      "CONCURRENCY"              = "32"
+      "NODE_ENV"                 = "production"
+      "SKIP_PUBLISHING"          = "false"
+      "SQS_PUBLISHING_QUEUE_URL" = data.terraform_remote_state.shared.outputs.sqs_multihashes_topic.url   
+  }  
+
+  indexer_image_url = "505595374361.dkr.ecr.us-west-2.amazonaws.com/indexer-lambda:latest"
+}
