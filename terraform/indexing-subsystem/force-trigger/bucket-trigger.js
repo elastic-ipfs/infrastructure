9538a0c0a5242a6623a01b671583888acacdc6b9
Francisco Cardoso <francardoso@outlook.com>
2022-03-25 09:31:02 -0300

    Send to queue

--- terraform/indexing-subsystem/force-trigger/bucket-trigger.js
+++ terraform/indexing-subsystem/force-trigger/bucket-trigger.js
@@ -1,5 +1,6 @@
-const { S3 } = require('aws-sdk')
+const { S3, SQS } = require('aws-sdk')
 const s3 = new S3()
+const sqs = new SQS()
 
 async function* listAllKeys(opts) {
   opts = { ...opts }
@@ -11,7 +12,7 @@ async function* listAllKeys(opts) {
 }
 
 const opts = {
-  Bucket: process.env.SOURCE_BUCKET_NAME /* required */,
+  Bucket: process.env.SOURCE_BUCKET_NAME,
   // ContinuationToken: 'STRING_VALUE',
   // Delimiter: 'STRING_VALUE',
   // EncodingType: url,
@@ -22,10 +23,32 @@ const opts = {
   // StartAfter: 'STRING_VALUE'
 }
 
+async function sendIndexSQSMessage(bucketName, fileKey) {
+  const message = `s3://${bucketName}/${fileKey}`
+  console.log(message)
+
+  var params = {
+    MessageBody: message,
+    // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+    // MessageGroupId: "Group1",  // Required for FIFO queues
+    QueueUrl: process.env.SQS_QUEUE_URL,
+  }
+
+  sqs.sendMessage(params, function (err, data) {
+    if (err) {
+      console.log('Error', err)
+    } else {
+      console.log('Success', data.MessageId)
+    }
+  })
+}
+
 async function main() {
   // using for of await loop
   for await (const data of listAllKeys(opts)) {
-    console.log(data.Contents)
+    // console.log(data.Contents)
+    sendIndexSQSMessage(opts.Bucket, data.Contents[0].Key)
   }
 }
+
 main()
