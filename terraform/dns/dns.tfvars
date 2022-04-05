5ff924588309ec4100cf0e4d1da2e5ff90d7985c
Francisco Cardoso <francardoso@outlook.com>
2022-04-05 17:35:32 -0300

    [READY] - [NA-000] - Alias (#88)
    * Changing DNS config to ALIAS instead of CNAME
    
    * dns tfvars with loadbalancer info

--- terraform/dns/dns.tfvars
+++ terraform/dns/dns.tfvars
@@ -4,4 +4,5 @@ region = "us-west-2"
 existing_aws_zone=true
 aws_domain_name = "ipfs-elastic-provider-aws.com"
 subdomains_bitwsap_loadbalancer="peer"
-bitswap_load_balancer_hostname = "ad62259e461cc4df986d3d01bd51140f-2139506693.us-west-2.elb.amazonaws.com"
+bitswap_load_balancer_dns = "dualstack.ad62259e461cc4df986d3d01bd51140f-2139506693.us-west-2.elb.amazonaws.com"
+bitswap_load_balancer_hosted_zone = "Z1H1FL5HABSF5"
--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -11,7 +11,7 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.38"
-    }    
+    }
   }
 
   required_version = ">= 1.0.0"
@@ -41,10 +41,10 @@ provider "aws" {
 }
 
 module "dns_route53" {
-  source                          = "../modules/dns-route53"
-  existing_zone                   = var.existing_aws_zone
-  domain_name                     = var.aws_domain_name
-  subdomains_bitwsap_loadbalancer = var.subdomains_bitwsap_loadbalancer
-  bitswap_load_balancer_hostname  = var.bitswap_load_balancer_hostname
+  source                            = "../modules/dns-route53"
+  existing_zone                     = var.existing_aws_zone
+  domain_name                       = var.aws_domain_name
+  subdomains_bitwsap_loadbalancer   = var.subdomains_bitwsap_loadbalancer
+  bitswap_load_balancer_dns         = var.bitswap_load_balancer_dns
+  bitswap_load_balancer_hosted_zone = var.bitswap_load_balancer_hosted_zone
 }
-
