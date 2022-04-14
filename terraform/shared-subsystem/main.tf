612d5f00bbd3766914a9245f6e3df0eeb4af8711
Francisco Cardoso <francardoso@outlook.com>
2022-04-14 15:10:54 -0300

    Multihashes DLQ

--- terraform/shared-subsystem/main.tf
+++ terraform/shared-subsystem/main.tf
@@ -40,6 +40,20 @@ resource "aws_sqs_queue" "multihashes_topic" {
   name                       = "multihashes-topic"
   message_retention_seconds  = 86400 # 1 day
   visibility_timeout_seconds = 300   # 5 min
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.multihashes_topic_dlq.arn
+    maxReceiveCount     = 2
+  })
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = ["${aws_sqs_queue.multihashes_topic_dlq.arn}"]
+  })
+}
+
+resource "aws_sqs_queue" "multihashes_topic_dlq" {
+  name                       = "multihashes-topic-dlq"
+  message_retention_seconds  = 1209600 # 14 days (Max quota)
+  visibility_timeout_seconds = 300
 }
 
 module "dynamodb" {
