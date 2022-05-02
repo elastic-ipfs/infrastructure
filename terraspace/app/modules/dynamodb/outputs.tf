6718bd48cd2124c702c9cb4899214cde0fd85591
Francisco Cardoso <francardoso@outlook.com>
2022-05-02 15:41:50 -0300

    terraspace fmt

--- terraspace/app/modules/dynamodb/outputs.tf
+++ terraspace/app/modules/dynamodb/outputs.tf
@@ -4,16 +4,16 @@ output "dynamodb_blocks_policy" {
 }
 
 output "dynamodb_car_policy" {
-  value = aws_iam_policy.dynamodb_car_policy
+  value       = aws_iam_policy.dynamodb_car_policy
   description = "Policy for allowing all Dynamodb Actions for cars table"
 }
 
 output "blocks_table" {
-  value = aws_dynamodb_table.blocks_table
+  value       = aws_dynamodb_table.blocks_table
   description = "Dynamodb blocks table object"
 }
 
 output "cars_table" {
-  value = aws_dynamodb_table.cars_table
+  value       = aws_dynamodb_table.cars_table
   description = "Dynamodb cars table object"
 }
