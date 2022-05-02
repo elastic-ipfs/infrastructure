2f530aa15a14126d12bbfdd95ba6b0e60363eab5
Francisco Cardoso <francardoso@outlook.com>
2022-05-02 09:46:50 -0300

    Adding descriptions to outputs to be complient with tflint

--- terraspace/app/modules/dynamodb/outputs.tf
+++ terraspace/app/modules/dynamodb/outputs.tf
@@ -1,15 +1,19 @@
 output "dynamodb_blocks_policy" {
-  value = aws_iam_policy.dynamodb_blocks_policy
+  value       = aws_iam_policy.dynamodb_blocks_policy
+  description = "Policy for allowing all Dynamodb Actions for blocks table"
 }
 
 output "dynamodb_car_policy" {
   value = aws_iam_policy.dynamodb_car_policy
+  description = "Policy for allowing all Dynamodb Actions for cars table"
 }
 
 output "blocks_table" {
   value = aws_dynamodb_table.blocks_table
+  description = "Dynamodb blocks table object"
 }
 
 output "cars_table" {
   value = aws_dynamodb_table.cars_table
+  description = "Dynamodb cars table object"
 }
--- terraspace/app/stacks/shared/outputs.tf
+++ terraspace/app/stacks/shared/outputs.tf
@@ -5,6 +5,7 @@ output "ipfs_peer_bitswap_config_bucket" {
     arn    = aws_s3_bucket.ipfs_peer_bitswap_config.arn
     region = aws_s3_bucket.ipfs_peer_bitswap_config.region
   }
+  description = "The bucket which contains configurations that some elastic provider apps require"
 }
 
 output "sqs_multihashes_topic" {
@@ -12,6 +13,7 @@ output "sqs_multihashes_topic" {
     url = aws_sqs_queue.multihashes_topic.url
     arn = aws_sqs_queue.multihashes_topic.arn
   }
+  description = "This queue is supposed to be used for triggering publisher lambda (content)"
 }
 
 output "dynamodb_blocks_policy" {
@@ -19,6 +21,7 @@ output "dynamodb_blocks_policy" {
     name = module.dynamodb.dynamodb_blocks_policy.name,
     arn  = module.dynamodb.dynamodb_blocks_policy.arn,
   }
+  description = "Policy for allowing all Dynamodb Actions for blocks table"
 }
 
 output "dynamodb_car_policy" {
@@ -26,6 +29,7 @@ output "dynamodb_car_policy" {
     name = module.dynamodb.dynamodb_car_policy.name,
     arn  = module.dynamodb.dynamodb_car_policy.arn,
   }
+  description = "Policy for allowing all Dynamodb Actions for cars table"
 }
 
 output "s3_config_peer_bucket_policy_read" {
@@ -33,6 +37,7 @@ output "s3_config_peer_bucket_policy_read" {
     name = aws_iam_policy.s3_config_peer_bucket_policy_read.name,
     arn  = aws_iam_policy.s3_config_peer_bucket_policy_read.arn,
   }
+  description = "Policy for allowing read action for configuration bucket"
 }
 
 output "sqs_multihashes_policy_send" {
@@ -40,6 +45,7 @@ output "sqs_multihashes_policy_send" {
     name = aws_iam_policy.sqs_multihashes_policy_send.name,
     arn  = aws_iam_policy.sqs_multihashes_policy_send.arn,
   }
+  description = "Policy for allowing send messages to multihashes sqs queue"
 }
 
 output "sqs_multihashes_policy_receive" {
@@ -47,6 +53,7 @@ output "sqs_multihashes_policy_receive" {
     name = aws_iam_policy.sqs_multihashes_policy_receive.name,
     arn  = aws_iam_policy.sqs_multihashes_policy_receive.arn,
   }
+  description = "Policy for allowing receive messages from multihashes sqs queue"
 }
 
 
@@ -55,6 +62,7 @@ output "sqs_multihashes_policy_delete" {
     name = aws_iam_policy.sqs_multihashes_policy_delete.name,
     arn  = aws_iam_policy.sqs_multihashes_policy_delete.arn,
   }
+  description = "Policy for allowing delete messages from multihashes sqs queue"
 }
 
 output "s3_dotstorage_prod_0_policy_read" {
@@ -62,4 +70,5 @@ output "s3_dotstorage_prod_0_policy_read" {
     name = aws_iam_policy.s3_dotstorage_prod_0_policy_read.name,
     arn  = aws_iam_policy.s3_dotstorage_prod_0_policy_read.arn,
   }
+  description = "Policy for allowing reading messages from existing bucket called 'dotstorage_prod_0'"
 }
