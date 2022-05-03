b0d20907eb44164b3b1162963c68ddbc6eb8c92e
Francisco Cardoso <francardoso@outlook.com>
2022-05-03 15:40:37 -0300

    Version helper

--- terraspace/app/stacks/shared/main.tf
+++ terraspace/app/stacks/shared/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.38"
+      version = "<%= provider_aws_version %>"
     }
   }
 
---
+++ terraspace/config/helpers/version_helper.rb
@@ -0,0 +1,5 @@
+module Terraspace::Project::VersionHelper
+    def provider_aws_version
+        "~> 3.38"
+    end
+end
