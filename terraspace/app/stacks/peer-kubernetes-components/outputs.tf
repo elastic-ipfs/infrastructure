46954dc6ffb8b57a1289e4fe9366b41d69060a97
Francisco Cardoso <francardoso@outlook.com>
2022-05-10 09:47:50 -0300

    Removing output for loop

--- terraspace/app/stacks/peer-kubernetes-components/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "iam_roles" {
-  value = {
-    for iam_role_arn, attributes in module.iam_assumable_role_admin :
-    iam_role_arn => attributes.iam_role_arn
-  }
-  description = "All roles managed by this stack"
-}
