7d5b93da89c10d9f6cdfa0fa20a2ffbd4d9042a2
blucas <blucas@users.noreply.github.com>
2022-05-13 16:45:38 -0400

    tflint fixes

--- terraspace/app/stacks/dns/outputs.tf
+++ terraspace/app/stacks/dns/outputs.tf
@@ -1,3 +1,4 @@
 output "bitswap_loadbalancer_domain" {
-  value = aws_route53_record.peer_bitswap_load_balancer.name
+  value       = aws_route53_record.peer_bitswap_load_balancer.name
+  description = "Domain name for bitswap peer"
 }
