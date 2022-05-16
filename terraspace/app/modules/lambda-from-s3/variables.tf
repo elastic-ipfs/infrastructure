6de1f9e8d2824e69e1c8fd33efa902d86c672158
Francisco Cardoso <francardoso@outlook.com>
2022-05-16 16:08:29 -0300

    fmt

--- terraspace/app/modules/lambda-from-s3/variables.tf
+++ terraspace/app/modules/lambda-from-s3/variables.tf
@@ -1,10 +1,10 @@
 variable "lambda" {
   type = object({
-    name                           = string
-    image_uri                      = string
-    environment_variables          = map(string)
-    memory_size                    = number
-    timeout                        = number
+    name                  = string
+    image_uri             = string
+    environment_variables = map(string)
+    memory_size           = number
+    timeout               = number
     policies_list = list(object({
       name = string,
       arn  = string,
@@ -20,7 +20,7 @@ variable "bucket" {
   })
 }
 
-variable custom_metrics {
-  type = list(string)
+variable "custom_metrics" {
+  type    = list(string)
   default = []
 }
