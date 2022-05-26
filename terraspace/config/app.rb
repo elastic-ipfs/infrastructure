339186f49687c648a32e900de1474092db732814
Francisco Cardoso <francardoso@outlook.com>
2022-05-26 16:23:38 -0300

    Publishing stack DNS dependency

--- terraspace/config/app.rb
+++ terraspace/config/app.rb
@@ -1,5 +1,5 @@
 # Docs: https://terraspace.cloud/docs/config/reference/
 Terraspace.configure do |config|
   config.logger.level = :info
-  config.all.exclude_stacks = ["bucket-to-indexer-lambda", "dns"] # DNS depends on Bitswap Peer ELB URL (Managed by Kubernetes)
+  config.all.exclude_stacks = ["bucket-to-indexer-lambda", "dns", "publishing"] # DNS depends on Bitswap Peer ELB URL (Managed by Kubernetes). Publishing depends on DNS
 end
