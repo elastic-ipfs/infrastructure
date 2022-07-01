8e73790f01b50c9dc50bcf46a69855621dba4c8e
Francisco Cardoso <francardoso@outlook.com>
2022-07-01 14:21:31 -0300

    Create and validate cert

---
+++ terraspace/app/stacks/dns/certificate.tf
@@ -0,0 +1,33 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name               = "${var.subdomains_bitwsap_loadbalancer}.${var.aws_domain_name}"
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.create_zone ? aws_route53_zone.hosted_zone[0].zone_id : data.aws_route53_zone.hosted_zone[0].zone_id 
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  timeouts {
+    create = "45m"
+  }
+}
