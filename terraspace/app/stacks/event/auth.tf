69b9b779f2c1f551b61f8c956d9ff69edf7df176
Francisco Cardoso <francardoso@outlook.com>
2022-08-03 11:35:15 -0300

    Improve policy description

--- terraspace/app/stacks/event/auth.tf
+++ terraspace/app/stacks/event/auth.tf
@@ -68,7 +68,7 @@ EOF
 
 resource "aws_iam_policy" "read_event_target_credentials_param" {
   name        = var.read_event_target_credentials_param_policy_name
-  description = "Policy for allowing reading secret of event target credential"
+  description = "Policy for allowing reading event target credential secret"
   policy      = <<EOF
 {
     "Version": "2012-10-17",
