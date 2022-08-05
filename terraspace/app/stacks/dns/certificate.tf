d60ccd0978330a690f5bf4fc2aa165f551649ddb
Francisco Cardoso <francardoso@outlook.com>
2022-08-05 16:53:01 -0300

    Imported certificate to AWS ACM

--- terraspace/app/stacks/dns/certificate.tf
+++ terraspace/app/stacks/dns/certificate.tf
@@ -1,20 +1,23 @@
+data "http" "cloudflare_certificate_chain" {
+  url = "https://developers.cloudflare.com/ssl/static/origin_ca_rsa_root.pem"
+}
+
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
 }
 
-resource "tls_cert_request" "example" {
-  private_key_pem = tls_private_key.private_key.private_key_pem # TODO: Creio que já pego esse direto pra mandar para AWS
-
-#   subject {
-#     common_name  = ""
-#     organization = "Terraform Test"
-#   }
+resource "tls_cert_request" "cert_request" {
+  private_key_pem = tls_private_key.private_key.private_key_pem
 }
 
-resource "cloudflare_origin_ca_certificate" "example" {
-  csr                = tls_cert_request.example.cert_request_pem
-#   hostnames          = [ "example.com" ]
-  hostnames          = [ "${var.bitswap_peer_record.name}.${var.cf_domain_name}" ]
-  request_type       = "origin-rsa"
-#   requested_validity = 7
+resource "cloudflare_origin_ca_certificate" "cert" {
+  csr          = tls_cert_request.cert_request.cert_request_pem
+  hostnames    = ["${var.bitswap_peer_record.name}.${var.cf_domain_name}"]
+  request_type = "origin-rsa"
+}
+
+resource "aws_acm_certificate" "cert" {
+  private_key       = tls_private_key.private_key.private_key_pem
+  certificate_body  = cloudflare_origin_ca_certificate.cert.certificate
+  certificate_chain = data.http.cloudflare_certificate_chain.body
 }
