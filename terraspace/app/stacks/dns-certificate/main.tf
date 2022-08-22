2c913e7a345590a293a79222f4d3752b14fe3251
Francisco Cardoso <francardoso@outlook.com>
2022-08-22 15:25:46 -0300

    New test forcing to get some updates

--- terraspace/app/stacks/dns-certificate/main.tf
+++ terraspace/app/stacks/dns-certificate/main.tf
@@ -35,19 +35,3 @@ resource "tls_private_key" "private_key" {
 resource "tls_cert_request" "cert_request" {
   private_key_pem = tls_private_key.private_key.private_key_pem
 }
-
-resource "cloudflare_origin_ca_certificate" "cert" {
-  csr          = tls_cert_request.cert_request.cert_request_pem
-  hostnames    = ["${var.bitswap_peer_record_name}.${var.cf_domain_name}"]
-  request_type = "origin-rsa"
-}
-
-resource "aws_acm_certificate" "cert" {
-  private_key       = tls_private_key.private_key.private_key_pem
-  certificate_body  = cloudflare_origin_ca_certificate.cert.certificate
-  certificate_chain = data.http.cloudflare_certificate_chain.body
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
--- terraspace/app/stacks/dns/tfvars/base.tfvars
+++ terraspace/app/stacks/dns/tfvars/base.tfvars
@@ -7,4 +7,3 @@ cf_domain_name = <%= output('dns-certificate.cf_domain_name', mock: '') %>
 bitswap_peer_record_name = <%= output('dns-certificate.bitswap_peer_record_name', mock: {}) %>
 # Dependency is not mapped based on outputs, so explicitly call out the dependency
 # bitswap_load_balancer_dns variable will always be wrong when starting this environment from scratch. Remember to update it.
-<% depends_on('peer-kubernetes-components') %>
