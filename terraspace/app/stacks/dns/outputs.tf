3f30848ef4c644fc0ee20c9cffd54b95094b6016
Francisco Cardoso <francardoso@outlook.com>
2022-08-30 11:21:45 -0300

    Change dns output to newer DNS record

--- terraspace/app/stacks/dns/outputs.tf
+++ terraspace/app/stacks/dns/outputs.tf
@@ -1,5 +1,4 @@
-# TODO: Replace with new domain to start advertising new address through publishing subsys. Will do after confirming with #storetheindex
 output "bitswap_loadbalancer_domain" {
-  value       = aws_route53_record.peer_bitswap_load_balancer.name
+  value       = "${cloudflare_record.bitswap_peer.name}.${data.cloudflare_zone.dns.name}"
   description = "Domain name for bitswap peer"
 }
