a54df69200804459f59f1715c766777a4eba69bc
fabiolrodriguez <fabio.rodriguez@nearform.com>
2022-09-27 16:19:20 -0300

    cleanup variables

--- terraspace/app/stacks/autocannon/tfvars/base.tfvars
+++ terraspace/app/stacks/autocannon/tfvars/base.tfvars
@@ -4,14 +4,4 @@ security_group_id = "sg-0da8ec52f04fcca8d"
 key_name          = "management-ipfs-elastic"
 ##
 autocannon_ami_name = "autocannon"
-source_bucket_name     = "dotstorage-<%= expansion(':ENV') %>-0"
 ec2_instance_name      = "<%= expansion(':ENV') %>-ep-autocannon"
-s3_client_aws_region   = "us-east-2"
-sqs_queue_url          = "<%= output('indexing.sqs_indexer_topic', mock: '').to_ruby['url'] %>"
-s3_suffix              = ".car"
-log_after_value_files  = 10000
-policies_list = [
-  <%= output('shared.s3_dotstorage_policy_read', mock: {}) %>,
-  <%= output('indexing.sqs_indexer_policy_send', mock: {}) %>,
-]
-read_only_mode     = "disabled"
--- terraspace/app/stacks/autocannon/variables.tf
+++ terraspace/app/stacks/autocannon/variables.tf
@@ -23,50 +23,6 @@ variable "autocannon_ami_name" {
   description = "Name of image (AMI) which contains 'autocannon' prepared to run"
 }
 
-variable "source_bucket_name" {
-  type        = string
-  description = "Name of bucket to read objects from"
-}
-
-variable "s3_client_aws_region" {
-  type        = string
-  description = "Which region is the source bucket"
-}
-
-variable "s3_prefix" {
-  type        = string
-  description = "Only read objects with this prefix"
-  default     = ""
-}
-
-variable "s3_suffix" {
-  type        = string
-  description = "Only read objects with this suffix"
-}
-
-variable "sqs_queue_url" {
-  type        = string
-  description = "indexer SQS queue URL"
-}
-
-variable "read_only_mode" {
-  type        = string
-  description = "Should be 'disable' for sending messages to SQS queue. Otherwise it will just read existing files from bucket"
-  default     = "enabled"
-}
-
-variable "file_await" {
-  type        = number
-  description = "How long to await between files. Useful for avoiding DB throttling"
-  default     = 0
-}
-
-variable "next_page_await" {
-  type        = number
-  description = "How long to await after fetching 1000 files. Useful for avoiding DB throttling"
-  default     = 0
-}
-
 variable "policies_list" {
   type = list(object({
     name = string,
