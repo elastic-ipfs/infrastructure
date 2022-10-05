59e8207221d2d9b325d957af678d3f0a378536cb
Francisco Cardoso <francardoso@outlook.com>
2022-10-05 14:55:50 -0300

    Removing SSH access

--- terraspace/app/stacks/peer-e2e-testing/main.tf
+++ terraspace/app/stacks/peer-e2e-testing/main.tf
@@ -9,10 +9,6 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
-data "aws_availability_zones" "azs" {
-  state = "available"
-}
-
 data "aws_ami" "peer_e2e_testing" {
   most_recent = true
 
@@ -33,9 +29,6 @@ resource "aws_instance" "peer_e2e_testing_runner" {
   ami                    = data.aws_ami.peer_e2e_testing.id
   instance_type          = "t3.medium"
   subnet_id              = var.subnet_id
-  availability_zone      = data.aws_availability_zones.azs.names[0]
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
