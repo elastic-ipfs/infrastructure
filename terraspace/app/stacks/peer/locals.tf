228a8016ab89cb29a7a5740eeaa64cc92fe5a909
Francisco Cardoso <francardoso@outlook.com>
2023-01-19 14:36:11 -0300

    fix range

--- terraspace/app/stacks/peer/locals.tf
+++ terraspace/app/stacks/peer/locals.tf
@@ -5,8 +5,8 @@ locals {
       open_all_eg = {
         description = "Open egress to all internet. Required for connecting with other Kubo/IPFS nodes, which can be running on any port"
         protocol    = "TCP"
-        from_port   = -1
-        to_port     = -1
+        from_port   = 0
+        to_port     = 65535
         type        = "egress"
         cidr_blocks = ["0.0.0.0/0"]
       }
