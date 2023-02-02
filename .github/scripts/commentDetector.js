7075d8f46b6d0dbed58a869c8878b36aea2e68f5
Francisco Cardoso <francardoso@outlook.com>
2023-02-02 17:02:39 -0300

    Get region and environment from user input (#207)
    * Get region and environment from user input
    
    * Run for multiregion/multienv
    
    * Remove old debug code

--- .github/scripts/commentDetector.js
+++ .github/scripts/commentDetector.js
@@ -1,6 +1,6 @@
-module.exports = async ({github, context}) => {
+module.exports = async ({ github, context }) => {
   const ref = process.env.GIT_REF
-  const {owner, repo} = context.repo
+  const { owner, repo } = context.repo
   const result = {
     proceed: "true",
     isPrComment: "false",
@@ -9,6 +9,16 @@ module.exports = async ({github, context}) => {
     requestPlan: "false",
     requestUp: "false",
   }
+  const validRegions = [
+    "us-east-1",
+    "us-east-2",
+    "us-west-2",
+  ]
+  const validEnvs = [
+    "dev",
+    "staging",
+    "prod",
+  ]
   if (['issue_comment'].includes(context.eventName)) {
     result.proceed = "false"
     if (context.payload.issue?.pull_request) {
@@ -18,12 +28,14 @@ module.exports = async ({github, context}) => {
       const body = context.payload.comment.body.toLowerCase().trim()
       const commandArray = body.split(/\s+/)
       // Valid Commands:
-      // - terraspace all plan
-      // - terraspace all up
-      // - terraspace plan [stack]
-      // - terraspace up [stack]
-      if (commandArray[0] === "terraspace" && commandArray.length === 3) {
-        result.program = "terraspace"
+      // - AWS_REGION=<value> TS_ENV=<value> terraspace all plan
+      // - AWS_REGION=<value> TS_ENV=<value> terraspace all up
+      // - AWS_REGION=<value> TS_ENV=<value> terraspace plan [stack]
+      // - AWS_REGION=<value> TS_ENV=<value> terraspace up [stack]
+      if (commandArray[2] === "terraspace" && commandArray.length === 5) {
+        result.aws_region = cleanAndValidateUserInput(commandArray[0], validRegions)
+        result.ts_env = cleanAndValidateUserInput(commandArray[1], validEnvs)
+        result.program = commandArray[2]
         const createStatusChecks = async () => {
           const res = await github.rest.checks.create({
             owner,
@@ -55,21 +67,21 @@ module.exports = async ({github, context}) => {
           result.commitStatusId = commitStatusRes.data.id
           // console.log(`commitStatusRes = ${JSON.stringify(commitStatusRes)}`)
         }
-        if (commandArray[1] === "all" && ["plan", "up"].includes(commandArray[2])) {
+        if (commandArray[3] === "all" && ["plan", "up"].includes(commandArray[4])) {
           await createStatusChecks()
 
           result.proceed = "true"
           result.cmd = "all"
-          result.subCmd = commandArray[2]
+          result.subCmd = commandArray[4]
 
           result.requestAllUp = result.subCmd === "up" ? "true" : "false"
           result.requestAllPlan = result.subCmd === "plan" ? "true" : "false"
-        } else if (["plan", "up"].includes(commandArray[1])) {
+        } else if (["plan", "up"].includes(commandArray[3])) {
           await createStatusChecks()
 
           result.proceed = "true"
-          result.cmd = commandArray[1]
-          result.subCmd = commandArray[2]
+          result.cmd = commandArray[3]
+          result.subCmd = commandArray[4]
 
           result.requestUp = result.cmd === "up" ? "true" : "false"
           result.requestPlan = result.cmd === "plan" ? "true" : "false"
@@ -79,4 +91,17 @@ module.exports = async ({github, context}) => {
     }
   }
   return result
+}
+
+/** 
+ * Gets value after '=' 
+ * Don't allow spaces, and escape special characters, to avoid injections 
+ */
+function cleanAndValidateUserInput(input, validValues) {
+  const cleanInput = input.trim().substring(input.indexOf("=") + 1)
+  if (validValues.includes(cleanInput)) {
+    return cleanInput
+  } else {
+    throw new Error(`Invalid value. Accepted values are: ${validValues.join(', ')}`)
+  }
 }
